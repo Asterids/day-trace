2813a906b4c3daa2d2190b79051a98a7e2c44357
Ruth Hill <ruthhill.e@gmail.com>
2018-03-05 21:59:00 -0500

    created sample chart within MyData component

--- client/components/MyData.jsx
+++ client/components/MyData.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { VictoryChart, VictoryLine, VictoryTheme, VictoryZoomContainer } from "victory"
+import { VictoryChart, VictoryGroup, VictoryBar, VictoryTheme, VictoryZoomContainer } from "victory"
 
 export default class MyData extends React.Component {
     constructor() {
       super();
       this.state = {
-        zoomDomain: { x: [new Date(2018, 1, 1), new Date(2018, 1, 23)] }
+        zoomDomain: { x: [new Date(2018, 0, 1), new Date(2018, 2, 5)] }
       };
     }
 
@@ -15,6 +15,37 @@ export default class MyData extends React.Component {
 
     render () {
       const userData = [
+        {'date': new Date(2018, 0, 1), 'Had caffeine': 0, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 0, 2), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 3), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 1},
+        {'date': new Date(2018, 0, 4), 'Had caffeine': 0, 'Had alcohol': 1, 'Had headache': 1},
+        {'date': new Date(2018, 0, 5), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 6), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 7), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 8), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 9), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 10), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 11), 'Had caffeine': 1, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 0, 12), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 13), 'Had caffeine': 0, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 0, 14), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 1},
+        {'date': new Date(2018, 0, 15), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 16), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 17), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 18), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 19), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 20), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 21), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 22), 'Had caffeine': 0, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 0, 23), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 24), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 25), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 26), 'Had caffeine': 1, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 0, 27), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 28), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 29), 'Had caffeine': 1, 'Had alcohol': 1, 'Had headache': 1},
+        {'date': new Date(2018, 0, 30), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 0, 31), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
         {'date': new Date(2018, 1, 1), 'Had caffeine': 1, 'Had alcohol': 1, 'Had headache': 0},
         {'date': new Date(2018, 1, 2), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
         {'date': new Date(2018, 1, 3), 'Had caffeine': 0, 'Had alcohol': 1, 'Had headache': 0},
@@ -37,12 +68,33 @@ export default class MyData extends React.Component {
         {'date': new Date(2018, 1, 20), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0},
         {'date': new Date(2018, 1, 21), 'Had caffeine': 1, 'Had alcohol': 1, 'Had headache': 0},
         {'date': new Date(2018, 1, 22), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
-        {'date': new Date(2018, 1, 23), 'Had caffeine': 1, 'Had alcohol': 1, 'Had headache': 0}
+        {'date': new Date(2018, 1, 23), 'Had caffeine': 1, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 1, 24), 'Had caffeine': 1, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 1, 25), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 1, 26), 'Had caffeine': 0, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 1, 27), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 1, 28), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 2, 1), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 0},
+        {'date': new Date(2018, 2, 2), 'Had caffeine': 0, 'Had alcohol': 1, 'Had headache': 0},
+        {'date': new Date(2018, 2, 3), 'Had caffeine': 0, 'Had alcohol': 0, 'Had headache': 1},
+        {'date': new Date(2018, 2, 4), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 1},
+        {'date': new Date(2018, 2, 5), 'Had caffeine': 1, 'Had alcohol': 0, 'Had headache': 0}
       ]
+
+      // let months = userData.forEach()
+      let actions = [
+        {'month': 'January', 'Had caffeine': 9, 'Had alcohol': 7, 'Had headache': 4},
+        {'month': 'February', 'Had caffeine': 12, 'Had alcohol': 10, 'Had headache': 3},
+        {'month': 'March', 'Had caffeine': 2, 'Had alcohol': 1, 'Had headache': 2}
+      ];
+
+      // for each month, for each field, sum occurrences
+
+
       return (
-        <div>
+        <div id="actions">
           <div className="chart-header">All Actions</div>
-          <VictoryChart width={600} height={470} scale={{ x: "time" }}
+          {/* <VictoryChart width={600} height={470} scale={{ x: "time" }}
             containerComponent={
               <VictoryZoomContainer
                 zoomDimension="x"
@@ -59,7 +111,29 @@ export default class MyData extends React.Component {
               x = 'date'
               y = 'Had caffeine'
             />
-        </VictoryChart>
+        </VictoryChart> */}
+          <VictoryChart viewBox={"0 0 500 300"}>
+            <VictoryGroup offset={20}
+              categories={{x: ["Jan", "Feb", "Mar"]}}
+              colorScale={["purple", "orange", "tomato"]}
+            >
+              <VictoryBar
+                data={actions}
+                x = 'month'
+                y = 'Had caffeine'
+              />
+              <VictoryBar
+                data={actions}
+                x = 'month'
+                y = 'Had alcohol'
+              />
+              <VictoryBar
+                data={actions}
+                x = 'month'
+                y = 'Had headache'
+              />
+            </VictoryGroup>
+          </VictoryChart>
         </div>
       )
     }
