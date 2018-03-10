006b9b4b3cfdb5b7dcf64bff73adf0aea8fe6e47
Ruth Hill <ruthhill.e@gmail.com>
2018-03-10 17:53:51 -0500

    Placeholder components About, Login, Signup

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SampleData from './SampleData';
 
-export default function Home () {
+export default function About () {
   return (
-    <div id="home">
-      <div className="welcome">
-        <section>
-          <h1>Welcome to DayTrace</h1>
-          <h3>Visualize data about your everyday</h3>
-          <h5>Mouse over the bars in the example chart below to see more information</h5>
-        </section>
-        <section>
-          <SampleData />
-        </section>
-        <br />
-        <section>
-          Learn more
-        </section>
-      </div>
+    <div id="about">
+      <section>
+        <h1>What is DayTrace?</h1>
+        <h3>Lorem ipsum paragraph</h3>
+        <h5>Another useful paragraph</h5>
+      </section>
+      <section>
+        <SampleData />
+      </section>
     </div>
   )
 }
--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import SampleData from './SampleData';
 
-export default function Home () {
+export default function Login () {
   return (
-    <div id="home">
-      <div className="welcome">
-        <section>
-          <h1>Welcome to DayTrace</h1>
-          <h3>Visualize data about your everyday</h3>
-          <h5>Mouse over the bars in the example chart below to see more information</h5>
-        </section>
-        <section>
-          <SampleData />
-        </section>
-        <br />
-        <section>
-          Learn more
-        </section>
-      </div>
+    <div id="login">
+      Login will happen here
     </div>
   )
 }
